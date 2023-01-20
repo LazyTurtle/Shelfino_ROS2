8157cd8e1d2629fe014bc8d540b8342142ace8ca
Jhonny Hueller <jh.hueller@gmail.com>
2023-01-20 16:10:46 +0100

    listening to obstacles

--- roadmap/src/roadmap_manager.cpp
+++ roadmap/src/roadmap_manager.cpp
@@ -6,6 +6,7 @@
 #include "geometry_msgs/msg/point32.hpp"
 #include "geometry_msgs/msg/polygon.hpp"
 #include "geometry_msgs/msg/polygon_stamped.hpp"
+#include "obstacles_msgs/msg/obstacle_array_msg.hpp"
 
 using std::placeholders::_1;
 
@@ -15,7 +16,10 @@ class RoadmapManager : public rclcpp::Node
     RoadmapManager()
     : Node("RoadmapManager"){
       border_subscriber = this->create_subscription<geometry_msgs::msg::PolygonStamped>(
-      "borders", 10, std::bind(&RoadmapManager::set_borders, this, _1));
+      "borders", 1, std::bind(&RoadmapManager::set_borders, this, _1));
+      
+      obstacles_subscriber = this->create_subscription<obstacles_msgs::msg::ObstacleArrayMsg>(
+        "obstacles", 1, std::bind(&RoadmapManager::set_obstacles, this, _1));
       
     }
 
@@ -25,9 +29,16 @@ class RoadmapManager : public rclcpp::Node
       borders = msg;
     }
 
+    void set_obstacles(const obstacles_msgs::msg::ObstacleArrayMsg::SharedPtr msg){
+      obstacles = msg;
+    }
+
     rclcpp::Subscription<geometry_msgs::msg::PolygonStamped>::SharedPtr border_subscriber;
+    rclcpp::Subscription<obstacles_msgs::msg::ObstacleArrayMsg>::SharedPtr obstacles_subscriber;
 
     std::shared_ptr<geometry_msgs::msg::PolygonStamped> borders;
+    std::shared_ptr<obstacles_msgs::msg::ObstacleArrayMsg> obstacles;
+
     
 };
 
