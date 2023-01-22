192268cabff270dddb144016bdca311b7daa0308
Jhonny Hueller <jh.hueller@gmail.com>
2023-01-22 09:59:54 +0100

    launch util

---
+++ shelfino_utils/launch/env.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='send_borders',
+            executable='send_borders',
+            name='borders_node'
+        ),
+        Node(
+            package='send_obstacles',
+            executable='send_obstacles',
+            name='obstacles_node'
+        ),
+        Node(
+            package='send_gates',
+            executable='send_gates',
+            name='gates_node'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            parameters=[{'use_sim_time': True}],
+            output='screen',
+        )
+    ])
