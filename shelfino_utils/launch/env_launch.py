5f1dcd06521f0822484be927e4d73a5497baf50b
Jhonny <jh.hueller@gmail.com>
2023-02-05 13:02:33 +0100

    fixing mapping and namespaces

--- shelfino_utils/launch/env_launch.py
+++ shelfino_utils/launch/env_launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
+remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
 
 def generate_launch_description():
     my_package_dir = get_package_share_directory("shelfino_utils")
@@ -30,6 +31,8 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             arguments=['-d', os.path.join(my_package_dir, 'config', 'testing.rviz')],
+            remappings = remappings,
+            namespace='shelfinoG',
             parameters=[{'use_sim_time': True}],
             output='screen',
         )
